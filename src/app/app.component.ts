2c7212322d6065d0e311b5bb9aeb1d67fc048039
Ganesh Ramar <ganeshramar98@gmail.com>
2025-02-12 16:37:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomEditorComponent } from './custom-editor/custom-editor.component';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CustomEditorComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'custom-editor-app';
+  editorForm = new FormGroup({
+    editorContent: new FormControl('') // Initialize FormControl
+  });
 }
